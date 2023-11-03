233bb70d746d851d21920c5442a4a8c84a45fda9
caringaljohnlloyd <130682372+caringaljohnlloyd@users.noreply.github.com>
2023-11-03 19:47:39 +0800

    .

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,7 +4,14 @@ import './registerServiceWorker'
 import router from './router'
 import axios from 'axios'
 import 'bootstrap'
+import{library} from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import {faFacebook, faGoogle,faTwitter} from '@fortawesome/free-brands-svg-icons';
 
+library.add(faFacebook, faGoogle, faTwitter);
 axios.defaults.baseURL="http://localhost:8080/"
 
-createApp(App).use(router).mount('#app')
+const app =createApp(App);
+app.use(router);
+app.mount('#app');
+app.component('font-awesome-icon',FontAwesomeIcon);
